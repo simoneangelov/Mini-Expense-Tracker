33ca2c8e5500e7e003d41bc0409131e833459746
Simone-Nicole Angelov <simone.nicole.angelov@gmail.com>
2024-11-04 09:29:10 -0600

    Initial project setup with Spring Boot

---
+++ src/main/java/com/example/mini_expense_tracker/MiniExpenseTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.mini_expense_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniExpenseTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniExpenseTrackerApplication.class, args);
+	}
+
+}
