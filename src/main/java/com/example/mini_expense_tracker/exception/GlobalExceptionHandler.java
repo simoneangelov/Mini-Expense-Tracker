f8388b8af780b02880496621652d5f0cbec33356
Simone-Nicole Angelov <simone.nicole.angelov@gmail.com>
2024-11-04 20:08:12 -0600

    Created global exceptions class

--- src/main/java/com/example/mini_expense_tracker/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/mini_expense_tracker/exception/GlobalExceptionHandler.java
@@ -1,5 +1,37 @@
 package com.example.mini_expense_tracker.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+// Provides structured JSON responses for exceptions.
+
+// @ControllerAdvice: Allows handling exceptions across the whole application.
+@ControllerAdvice
 public class GlobalExceptionHandler {
-    
+
+    // Handle ResourceNotFoundException
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", ex.getMessage());
+
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+
+    // Handle other exceptions
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleGlobalException(Exception ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", "An unexpected error occurred");
+
+        return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
