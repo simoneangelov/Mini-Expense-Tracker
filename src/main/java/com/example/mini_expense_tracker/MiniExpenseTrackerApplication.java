0a4cc01fb21643eba8f3f225b42266a0b6bfc08c
Simone-Nicole Angelov <simone.nicole.angelov@gmail.com>
2024-11-04 22:23:18 -0600

    More comments

--- src/main/java/com/example/mini_expense_tracker/MiniExpenseTrackerApplication.java
+++ src/main/java/com/example/mini_expense_tracker/MiniExpenseTrackerApplication.java
@@ -3,6 +3,9 @@ package com.example.mini_expense_tracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Entry point for the Spring Boot application! This is where the 'main' method is located.
+// When the application starts, it will run the 'main' method in this class.
+
 @SpringBootApplication
 public class MiniExpenseTrackerApplication {
 
